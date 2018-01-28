68d2ee480ffe602370162fe498f4e5f4fec3eae5
django <django@Gregorys-MacBook-Air.local>
2018-01-28 14:27:43 +0000

    Bootstrap

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['gestao-clientes2.herokuapp.com']
-
+ALLOWED_HOSTS = ['gestao-clientes2.herokuapp.com', 'localhost']
 
 # Application definition
 
