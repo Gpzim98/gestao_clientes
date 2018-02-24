cdce74e600461a962227c7220b8ab733178d0d65
django <django@Gregorys-MacBook-Air.local>
2018-02-24 14:29:57 +0000

    Produtividade

--- home/views.py
+++ home/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
 
+def calcular(v1, v2):
+    return v1 / v2
+
+
 def home(request):
-    return render(request, 'home.html')
+#    import pdb; pdb.set_trace()
+    value1 = 10
+    value2 = 20
+    res = calcular(value1, value2)
+    return render(request, 'home.html', {'result': res})
 
 
 def my_logout(request):
