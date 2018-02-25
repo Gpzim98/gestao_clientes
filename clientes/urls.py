4c71ebe74f196dd536a341f6f2320918c3f63ade
django <django@Gregorys-MacBook-Air.local>
2018-02-25 14:27:40 +0000

    UpdateView

--- clientes/urls.py
+++ clientes/urls.py
@@ -6,6 +6,7 @@ from .views import persons_delete
 from .views import PersonList
 from .views import PersonDetail
 from .views import PersonCreate
+from .views import PersonUpdate
 
 
 urlpatterns = [
@@ -15,5 +16,6 @@ urlpatterns = [
     path('delete/<int:id>/', persons_delete, name="persons_delete"),
     path('person_list', PersonList.as_view(), name='person_list_cbv'),
     path('person_detail/<int:pk>/', PersonDetail.as_view(), name='person_detail_cbv'),
+    path('person_update/<int:pk>/', PersonUpdate.as_view(), name='person_update_cbv'),
     path('person_create/', PersonCreate.as_view(), name='person_create_cbv'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -5,7 +5,9 @@ from .forms import PersonForm
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
 from django.utils import timezone
+from django.urls import reverse_lazy
 
 
 @login_required
@@ -64,3 +66,9 @@ class PersonCreate(CreateView):
     model = Person
     fields = ['first_name', 'last_name', 'age', 'salary', 'bio', 'photo']
     success_url = '/clientes/person_list'
+
+
+class PersonUpdate(UpdateView):
+    model = Person
+    fields = ['first_name', 'last_name', 'age', 'salary', 'bio', 'photo']
+    success_url = reverse_lazy('person_list_cbv')
