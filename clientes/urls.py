93d50e496a2a20c3ebe4088e95e3daef9a3ddc2a
django <django@Gregorys-MacBook-Air.local>
2018-02-25 14:55:27 +0000

    Delete

--- clientes/urls.py
+++ clientes/urls.py
@@ -7,6 +7,7 @@ from .views import PersonList
 from .views import PersonDetail
 from .views import PersonCreate
 from .views import PersonUpdate
+from .views import PersonDelete
 
 
 urlpatterns = [
@@ -17,5 +18,6 @@ urlpatterns = [
     path('person_list', PersonList.as_view(), name='person_list_cbv'),
     path('person_detail/<int:pk>/', PersonDetail.as_view(), name='person_detail_cbv'),
     path('person_update/<int:pk>/', PersonUpdate.as_view(), name='person_update_cbv'),
+    path('person_delete/<int:pk>/', PersonDelete.as_view(), name='person_delete_cbv'),
     path('person_create/', PersonCreate.as_view(), name='person_create_cbv'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -6,6 +6,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 from django.utils import timezone
 from django.urls import reverse_lazy
 
@@ -71,4 +72,9 @@ class PersonCreate(CreateView):
 class PersonUpdate(UpdateView):
     model = Person
     fields = ['first_name', 'last_name', 'age', 'salary', 'bio', 'photo']
+    success_url = reverse_lazy('person_list_cbv')
+
+
+class PersonDelete(DeleteView):
+    model = Person
     success_url = reverse_lazy('person_list_cbv')
