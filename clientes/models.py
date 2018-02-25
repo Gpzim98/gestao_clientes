d79a1a5d4c95363a6f82f2a4726c6403ab01d922
django <django@Gregorys-MacBook-Air.local>
2018-02-25 20:11:02 +0000

    Criando novo campo

--- clientes/models.py
+++ clientes/models.py
@@ -6,6 +6,7 @@ class Person(models.Model):
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
     salary = models.DecimalField(max_digits=5, decimal_places=2)
+    bio2 = models.TextField()
     bio = models.TextField()
     photo = models.ImageField(upload_to='clients_photos', null=True, blank=True)
 
