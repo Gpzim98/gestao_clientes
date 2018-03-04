64e56d9e6df3e37cff771e445e94367a85824605
django <django@Gregorys-MacBook-Air.local>
2018-03-04 13:43:01 +0000

    Relationsipts DB

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person, Documento, Venda, Produto
 
 
 admin.site.register(Person)
+admin.site.register(Documento)
+admin.site.register(Venda)
+admin.site.register(Produto)
