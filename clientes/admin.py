5411a54e65f53cb90d374ee51c9d4db4d1b97213
django <django@Gregorys-MacBook-Air.local>
2018-03-11 13:44:06 +0000

    Search field

--- clientes/admin.py
+++ clientes/admin.py
@@ -15,6 +15,8 @@ class PersonAdmin(admin.ModelAdmin):
     # exclude = ('bio', )
     list_filter = ('age', 'salary')
     list_display = ('doc', 'first_name', 'last_name', 'age', 'salary', 'bio', 'tem_foto')
+    search_fields = ('id', 'first_name')
+    autocomplete_fields = ['doc']
 
     def tem_foto(self, obj):
         if obj.photo:
@@ -27,12 +29,11 @@ class PersonAdmin(admin.ModelAdmin):
 
 class VendaAdmin(admin.ModelAdmin):
     readonly_fields = ('valor',)
-    raw_id_fields = ("pessoa",)
+    autocomplete_fields = ("pessoa", 'produtos')
     list_filter = ('pessoa__doc', 'desconto')
     list_display = ('id', 'pessoa', 'get_total', 'nfe_emitida')
     search_fields = ('id', 'pessoa__first_name', 'pessoa__doc__num_doc')
     actions = [nfe_emitida, nfe_nao_emitida]
-    filter_horizontal = ['produtos',]
 
     def total(self, obj):
         return obj.get_total()
@@ -42,9 +43,14 @@ class VendaAdmin(admin.ModelAdmin):
 
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = ('id', 'descricao', 'preco')
+    search_fields = ['id', 'descricao']
+
+
+class DocumentoAdmin(admin.ModelAdmin):
+    search_fields = ['num_doc']
 
 
 admin.site.register(Person, PersonAdmin)
-admin.site.register(Documento)
+admin.site.register(Documento, DocumentoAdmin)
 admin.site.register(Venda, VendaAdmin)
 admin.site.register(Produto, ProdutoAdmin)
